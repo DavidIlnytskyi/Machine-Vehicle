c60a350949bb411f666f7e24df02bbc20522b38c
Nazarious <116711001+Nazarious-ucu@users.noreply.github.com>
2023-03-22 12:50:08 +0200

    add new functions

--- vehicle_unittests.py
+++ vehicle_unittests.py
@@ -4,6 +4,29 @@ from vehicle import *
 class Tests(TestCase):
     '''This class provides tests for vehicle module'''
 
+    def test_vehicle(self):
+        v1 = Vehicle("Zorith", 2)
+        self.assertEqual(str(v1), "Zorith holds 2: [], free 2")
+        self.assertTrue(v1.is_empty())
+    
+        p1 = Passenger('Arman')
+        p2 = Passenger('Armana')
+        self.assertEqual(str(p1), "Arman is passenger of ...")
+        self.assertTrue(v1.add_passenger(p1))
+        self.assertEqual(p1.place, v1)
+        self.assertEqual(p1._Passenger__place, v1)
+        self.assertEqual(str(p1), "Arman is passenger of Zorith")
+        self.assertFalse(v1.add_passenger(p1))
+        self.assertTrue(v1.add_passenger(p2))
+
+        p3 = Passenger("Arcana")
+        self.assertFalse(v1.add_passenger(p3))
+        self.assertEqual(str(v1), "Zorith holds 2: ['Arman', 'Armana'], free 0")
+        # can remove the most recent passenger
+        self.assertEqual(v1.remove_passenger(), p2)
+        self.assertEqual(str(v1), "Zorith holds 2: ['Arman'], free 1")
+        self.assertTrue(v1.add_passenger(p3))
+
     def test_comparability(self):
         path = "Lviv - Stari Kuty"
         v1 = Vehicle("Zorith", 2)
@@ -13,7 +36,7 @@ class Tests(TestCase):
         self.assertFalse(v2.remove_passenger())
         self.assertTrue(v2.add_passenger(p2))
         self.assertEqual(str(v2), 'Bus holds 20: [\'Armana\'], free 19')
-        self.assertTrue(v1.add_passenger(p3))
+        self.assertEqual(v1.remove_passenger(), (p2, 'Bus is empty!'))
         self.assertEqual(v2, Bus('Bus', 20, path))
         self.assertNotEqual(v2, Bus('Bus', 20, 'Lviv - Tuziliv'))
         self.assertNotEqual(v2, v1)
